3bfd53a2a405ef18fa4a46c1b8fa0567ed681f3e
evilguy50 <10925997+evilguy50@users.noreply.github.com>
2022-12-28 13:20:22 -0800

    Switch to local texture_convert

---
+++ filters/texture_convert/texture_convert.py
@@ -0,0 +1,23 @@
+import layeredimage.io
+import zipfile
+import os
+
+for root, d_names,f_names in os.walk("."):
+    for f in f_names:
+        imgpath = os.path.join(root, f)
+        replaced = False
+        fSplit = f.split(".")
+        if len(fSplit) < 2:
+            continue
+        ext = fSplit[1]
+        if ext in ["pdn", "xcf", "psd"]:
+            replaced = True
+            img = layeredimage.io.openLayerImage(imgpath)
+            img.getFlattenLayers().save(imgpath.replace('.' + ext, '.png'))
+        if f.endswith(".kra"):
+            replaced = True
+            with zipfile.ZipFile(imgpath) as z:
+                with open(imgpath.replace('.kra', '.png'), 'wb') as f2:
+                    f2.write(z.read('preview.png'))
+        if replaced:
+            os.remove(imgpath)
